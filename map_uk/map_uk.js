5aea33970299857e9cc2bb0533d9db50d0ec78d2
Camden Gutzman <camdengutzman@gmail.com>
2015-11-27 17:04:03 -0700

    Changed projection.

--- map_uk/map_uk.js
+++ map_uk/map_uk.js
@@ -1,16 +1,30 @@
+/* global topojson */
 /* global d3 */
 (function(){
 	'use strict';	
 	
 	var width = 960;
 	var height = 1160;
+	
 		
 	d3.json("./assets/uk.json", function(error, uk) {
 		if (error) return console.error(error);
 
-  svg.append("path")
-      .datum(topojson.feature(uk, uk.objects.subunits))
-      .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+		var subunits = topojson.feature(uk, uk.objects.subunits);
+		var projection = d3.geo.albers()
+			.center([0, 55.4])
+			.rotate([4.4, 0])
+			.parallels([50, 60])
+			.scale(6000)
+			.translate([width / 2, height / 2]);
+							
+		var path = d3.geo.path()
+    					.projection(projection);
+						
+		svg.append("path")
+			.datum(subunits)
+			.attr("d", path);
+
 	});	
 	
 	var svg = d3.select("body").append("svg")
