cfac4496fd19099466ab6619b75cc26525e00c57
Camden Gutzman <camdengutzman@gmail.com>
2015-11-28 18:39:20 -0700

    Add location and country labels. Add styling.

--- map_uk/map_uk.js
+++ map_uk/map_uk.js
@@ -4,12 +4,9 @@
 	'use strict';	
 	
 	var width = 960;
-	var height = 1160;
-	
+	var height = 1160;	
 		
-	d3.json("./assets/uk.json", function(error, uk) {
-		if (error) return console.error(error);
-
+	d3.json('./assets/uk.json', function(error, uk) {
 		var subunits = topojson.feature(uk, uk.objects.subunits);
 		var projection = d3.geo.albers()
 			.center([0, 55.4])
@@ -21,14 +18,54 @@
 		var path = d3.geo.path()
     					.projection(projection);
 						
+		var svg = d3.select("body").append("svg")
+			.attr("width", width)
+			.attr("height", height);
+						
 		svg.append("path")
 			.datum(subunits)
 			.attr("d", path);
-
-	});	
-	
-	var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+			
+		svg.selectAll(".subunit")
+			.data(topojson.feature(uk, uk.objects.subunits).features)
+		.enter().append("path")
+			.attr("class", function(d) { return "subunit " + d.id; })
+			.attr("d", path);
+		
+		svg.append("path")
+			.datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) { return a !== b && a.id !== "IRL"; }))
+			.attr("d", path)
+			.attr("class", "subunit-boundary");
+		
+		svg.append("path")
+			.datum(topojson.mesh(uk, uk.objects.subunits, function(a, b) { return a === b && a.id === "IRL"; }))
+			.attr("d", path)
+			.attr("class", "subunit-boundary IRL");
+			
+		svg.append("path")
+			.datum(topojson.feature(uk, uk.objects.places))
+			.attr("d", path)
+			.attr("class", "place");
+			
+		svg.selectAll(".place-label")
+			.data(topojson.feature(uk, uk.objects.places).features)
+		.enter().append("text")
+			.attr("class", "place-label")
+			.attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+			.attr("dy", ".35em")
+			.text(function(d) { return d.properties.name; });
+			
+		svg.selectAll(".place-label")
+			.attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
+			.style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; });
+			
+		svg.selectAll(".subunit-label")
+			.data(topojson.feature(uk, uk.objects.subunits).features)
+		.enter().append("text")
+			.attr("class", function(d) { return "subunit-label " + d.id; })
+			.attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+			.attr("dy", ".35em")
+			.text(function(d) { return d.properties.name; });
+	});		
 	
 })();
